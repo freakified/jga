b9ba4d91b469f6dda816b3aeebb5b66df0312478
Dan Tilden <dtilden@sicom.com>
2013-12-01 22:41:10 -0500

    Initial animation working, backgrounds in place for scene 1

---
+++ Assets/Chapter 1 - Dreams/Scripts/SpriteFlicker.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// To be applied to a Sprite object
+/// </summary>
+public class SpriteFlicker : MonoBehaviour {
+
+	/// <summary>
+	/// The delay between each flicker, in seconds
+	/// </summary>
+	public float flickerDelay = 0.5f;
+
+	/// <summary>
+	/// The probability that a flicker occurs (between 0 and 1)
+	/// </summary>
+	public float flickerProbability = 0.2f;
+
+
+	private float timeSinceLastFlicker = 0;
+	private bool isCurrentlyFlickering = false;
+	private Color normalColor, flickerColor;
+
+	// Initialization
+	void Start () {
+		normalColor = new Color (1f, 1f, 1f, 1f);
+		flickerColor = new Color (1f, 1f, 1f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastFlicker += Time.deltaTime;
+
+		if (timeSinceLastFlicker > flickerDelay) {
+
+			// flicker for exactly one frame
+			if(!isCurrentlyFlickering) {
+				//should we actually flicker?
+				if(Random.value < flickerProbability) {
+					((SpriteRenderer)renderer).color = flickerColor;
+					isCurrentlyFlickering = true;
+				}
+
+			} else {
+				((SpriteRenderer)renderer).color = normalColor;
+				timeSinceLastFlicker = 0;
+				isCurrentlyFlickering = false;
+			}
+
+		}
+	}
+}
---
+++ Assets/_Common/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	/// <summary>
+	/// The speed.
+	/// </summary>
+	public float Speed = 5f;
+
+	private bool isKeyDown = false;
+
+	protected Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+	    
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			isKeyDown = true;
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			isKeyDown = false;
+		}
+
+		animator.SetBool ("IsStabbing", isKeyDown);
+	}
+}
