975673b45ee90c9607183f6d7eaab2aba4e43419
Dan Tilden <dtilden@sicom.com>
2014-05-31 09:09:13 -0400

    Added shoes with REAL PHYSICS

---
+++ Assets/_Common/Scripts/ShoelaceBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoelaceBehavior : MonoBehaviour {
+
+	public GameObject AttachedShoe;
+
+	private LineRenderer r;
+	private DistanceJoint2D j;
+
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<LineRenderer>();
+		j = AttachedShoe.GetComponent<DistanceJoint2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		r.SetPosition(0, j.connectedBody.transform.position);
+		r.SetPosition(1, AttachedShoe.transform.position);
+	}
+}
