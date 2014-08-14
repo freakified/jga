f44f83843857b77d2e1c4d0e943cc7c5f7042015
Dan Tilden <dtilden@sicom.com>
2014-08-14 07:53:24 -0400

    Added first Chapter 5 scene

---
+++ Assets/Chapter 5 - Your Tax Dollars At Work/Scripts/S501Puppeteer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class S501Puppeteer : CutscenePuppeteer {
+
+	private GameObject ChefTony;
+	private MusicPlayer mus;
+
+	//private Animator ctanim;
+
+	// Use this for initialization
+	void Start () {
+		// get all the objects we'll need for the cutscene 
+		ChefTony = GameObject.Find ("Chef Tony");
+		mus = GameObject.Find ("BGM").GetComponent<MusicPlayer>();
+		//ctanim = ChefTony.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	public void FixedUpdate () {
+		if (CurrentScene == 4) {
+			if (ChefTony.transform.position.x > 1.2f) {
+				ChefTony.GetComponent<PlayerFreeze>().Freeze();
+				StartCoroutine(FadeAndNext(Color.black, 2.0f, null));
+				nextScene();
+			}
+		}
+	}
+
+	public override void HandleSceneChange() {
+		if (CurrentScene == 4) {
+			ChefTony.GetComponent<PlayerFreeze>().UnFreeze();
+		}
+	}
+
+}
