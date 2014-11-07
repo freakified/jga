4e3ca672ea4670695e63b73eab15d8025eca9d0a
Dan Tilden <dtilden@sicom.com>
2014-11-07 10:05:56 -0500

    More credits added

---
+++ Assets/Chapter 7 - Epilogue/Scripts/S705Puppeteer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class S705Puppeteer : CutscenePuppeteer {
+
+	public float CreditsSpeed = 1.0f;
+
+	private GameObject credits;
+		
+	// Use this for initialization
+	void Start () {
+		credits = GameObject.Find("Credits");
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		if(CurrentScene == 1) {
+			Vector3 pos = credits.transform.position;
+			pos.y += CreditsSpeed * Time.deltaTime;
+			credits.transform.position = pos;
+		}
+	}
+
+	public override void HandleSceneChange() {
+
+	}
+
+}
